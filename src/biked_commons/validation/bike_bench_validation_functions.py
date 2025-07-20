a1e4221a1df32123a3efbfbdc1ff2bc3bd767d9b
Lyle <Lyleregenwetter@gmail.com>
2025-07-20 05:49:34 -0700

    re-add lost notebooks, test GA constraint inference

--- src/biked_commons/validation/bike_bench_validation_functions.py
+++ src/biked_commons/validation/bike_bench_validation_functions.py
@@ -121,10 +121,10 @@ class SeatStaySmallerThanRearWheelRadius(ValidationFunction):
         h = z-y
         g = torch.sqrt(h**2 + x**2 - 2*h*x*torch.cos(Seat_angle_rad))
         return (RD / 2) - g
-    
+
 class SeatTubeIntersectsRearWheel(ValidationFunction):
     def friendly_name(self) -> str:
-        return "Seat Tube Intersects Rear Wheel"
+        return "Seat Tube Intersects Rear Wheel2"
 
     def variable_names(self) -> List[str]:
         return ["CS textfield", "BB textfield","Seat tube length", "Seat stay junction0", "Seat angle","Seat tube diameter", "Seat tube type OHCLASS: 0", "Wheel cut", "Wheel diameter rear"]
@@ -132,15 +132,14 @@ class SeatTubeIntersectsRearWheel(ValidationFunction):
     def validate(self, designs: torch.tensor, eps = 1e-6) -> torch.tensor:
         CS_textfield, BB_textfield, Seat_tube_length, Seat_stay_junction0, Seat_angle, Seat_tube_diameter, Seat_tube_type, Wheel_cut, RD = designs[:, :len(self.variable_names())].T
         Seat_angle_rad = (Seat_angle * math.pi) / 180
-        x = Seat_tube_length-(BB_textfield/torch.sin(Seat_angle_rad))-Seat_stay_junction0
-        y = BB_textfield/torch.tan(Seat_angle_rad)
-        z = torch.sqrt(torch.clip((CS_textfield ** 2)-(BB_textfield ** 2), min=eps))
-        h = z-y
-        g = torch.sqrt(h**2 + x**2 - 2*h*x*torch.cos(Seat_angle_rad))
-        f = torch.asin((h*torch.sin(Seat_angle_rad))/g)
-        j = g*torch.sin(f)
+        x = Seat_tube_length-(BB_textfield/torch.sin(Seat_angle_rad))-Seat_stay_junction0 # length along seat tube from stay function to horiz interc of wheel
+        y = BB_textfield/torch.tan(Seat_angle_rad)                                      #y distance from the above intercept point to bb
+        z = torch.sqrt(torch.clip((CS_textfield ** 2)-(BB_textfield ** 2), min=eps))    #Horiz distance from axle to BB
+        h = z-y                                                                         #Horiz distance from axle to st intercept
+        # g = torch.sqrt(h**2 + x**2 - 2*h*x*torch.cos(Seat_angle_rad))                   #SS Length
+        j = (h*torch.sin(Seat_angle_rad))
         
-        mask = (Seat_tube_type == 1)
+        mask = (Seat_tube_type == 1)                                                    #If aero tube, use logic based on wheel cut
         #q_true = j - (Seat_tube_diameter / 2) + ((Wheel_cut-RD) / 2)
         q_true = torch.where(Wheel_cut < RD, j - 40.9, (RD / 2) + ((Wheel_cut - RD) / 2))
         q_false = j - (Seat_tube_diameter/2) 
@@ -175,16 +174,14 @@ class RearWheelCutoutSeversSeatTube(ValidationFunction):
     def variable_names(self) -> List[str]:
         return ["CS textfield", "BB textfield","Seat tube length", "Seat stay junction0", "Seat angle", "Seat tube type OHCLASS: 0", "Wheel cut"] # replace Seat tube type  (mixed df) with Seat tube type OHCLASS: 0 (dataset)
 
-    def validate(self, designs: torch.tensor, eps = 1e-6) -> torch.tensor:
+    def validate(self, designs: torch.tensor, eps = 1e-6) -> torch.tensor: #same logic as: SeatTubeIntersectsRearWheel
         CS_textfield, BB_textfield, Seat_tube_length, Seat_stay_junction0, Seat_angle, Seat_tube_type, Wheel_cut = designs[:, :len(self.variable_names())].T
         Seat_angle_rad = (Seat_angle * math.pi) / 180
         x = Seat_tube_length-(BB_textfield/torch.sin(Seat_angle_rad))-Seat_stay_junction0
         y = BB_textfield/torch.tan(Seat_angle_rad)
         z = torch.sqrt(torch.clip((CS_textfield ** 2)-(BB_textfield ** 2), min=eps))
         h = z-y
-        g = torch.sqrt(h**2 + x**2 - 2*h*x*torch.cos(Seat_angle_rad))
-        f = torch.asin((h*torch.sin(Seat_angle_rad))/g)
-        j = g*torch.sin(f)
+        j = (h*torch.sin(Seat_angle_rad))
         
         mask = (Seat_tube_type == 1) # ==1 when using dataset, ==0 when using mixeddf
         #q_true = j - (Seat_tube_diameter / 2) + ((Wheel_cut-RD) / 2)
@@ -305,13 +302,14 @@ class DownTubeIntersectsFrontWheel(ValidationFunction):
             "Stack", "Head tube length textfield", "Head tube lower extension2", "Head angle", "DT Length", "BB textfield", "FORK0R", "Wheel diameter front", "Wheel diameter rear", "Down tube diameter",
         ]
 
-    def validate(self, designs: torch.Tensor) -> torch.tensor:
+    def validate(self, designs: torch.Tensor, eps=1e-6) -> torch.tensor:
         (stack, htl, htlx, head_angle, dt_len, bb_drop, fork0r, wdf, wdr, dt_dia) = designs[:, :len(self.variable_names())].T
 
         theta = head_angle * math.pi / 180.0  # radians
+        theta = torch.clip(theta, min=eps)
 
         DTJY = stack - ((htl-htlx)*torch.sin(theta))
-        DTJX = torch.sqrt((dt_len**2)-(DTJY**2))
+        DTJX = torch.sqrt(torch.clip((dt_len**2)-(DTJY**2), min=0))
 
         FBBD = bb_drop - wdr/2 + wdf/2  # y coordinate of front wheel axle, relative to BB, BB is (0,0)
 
